2f41b9b1e6e254fabb18bcb0f6a8dbb3b26c5fa2
AbdelrahmanTarek97 <Abdelrahman_Tarek251@hotmail.com>
2021-11-12 16:40:22 +0200

    Adding CORS to project

--- handler.js
+++ handler.js
@@ -1,6 +1,25 @@
+// Import serverless for deploying the project as serverless functions
 const serverless = require("serverless-http");
+// Import express for easier middleware setup and API exposure
 const express = require("express");
+// Import CORS middleware to enable cors
+const cors = require("cors");
+
+// Initalize an express App!
 const app = express();
+
+// Enable CORS middleware
+/*
+Cross-origin resource sharing is a mechanism
+that allows restricted resources on a web page
+to be requested from another domain outside 
+the domain
+*/
+app.use(cors());
+
+// Use the JSON middleware which converts JSON content into Objects and vice-versa to allow for easier request processing
+app.use(express.json());
+
 
 app.get("/", (req, res, next) => {
   return res.status(200).json({
@@ -20,4 +39,5 @@ app.use((req, res, next) => {
   });
 });
 
+// Expose APIs that are to be deployed by serverless
 module.exports.handler = serverless(app);
